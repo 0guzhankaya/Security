f20e736b9d1445386c45902ea73c6e68a0742772
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-11-10 18:33:17 +0300

    024-In-Memory Cache added in Product.

--- AuthServer.API/Controllers/ProductController.cs
+++ AuthServer.API/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Security.Core.Dtos;
 using Security.Core.Models;
 using Security.Core.Services;
@@ -12,21 +13,57 @@ namespace AuthServer.API.Controllers
 	public class ProductController : CustomBaseController
 	{
 		private readonly IGenericService<Product, ProductDto> _productService;
+		private readonly IMemoryCache _memoryCache;
 
-		public ProductController(IGenericService<Product, ProductDto> productService)
+		public ProductController(IGenericService<Product, ProductDto> productService, IMemoryCache memoryCache)
 		{
 			_productService = productService;
+			_memoryCache = memoryCache;
 		}
 
 		[HttpGet]
 		public async Task<IActionResult> GetProduct()
 		{
+			// time key'ine sahip cachelenecek data'yı almak ister,
+			// data eğer yoksa oluşturur.
+			_memoryCache.GetOrCreate<string>("time", entry =>
+			{
+				return DateTime.Now.ToString();
+			});
+
+			_memoryCache.TryGetValue("callback", out var callback);
+			var callbackCache = callback; // gösterilmesi için.
+
+			// var cache = _memoryCache.Get<string>("time");
 			return ActionResultInstance(await _productService.GetAllAsync());
 		}
 
 		[HttpPost]
 		public async Task<IActionResult> SaveProduct(ProductDto productDto)
 		{
+			// 1.Yol
+			if (String.IsNullOrEmpty(_memoryCache.Get<string>("time")))
+			{
+				_memoryCache.Set<string>("time", DateTime.Now.ToString());
+			}
+
+			// 2.Yol : Önerilir.
+			// time cache'ini alabilirse geri hem true döner hem de timeCache'e time key'ine sahip değeri cache'i atar.
+			// eğer true dönerse, - cache varsa - timeCache ile ilgili cache değeri alınıp kullanılabilir.
+			if (!_memoryCache.TryGetValue("time", out var timeCache))
+			{
+				MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+				options.SlidingExpiration = TimeSpan.FromSeconds(10);
+				options.AbsoluteExpiration = DateTime.Now.AddSeconds(30);
+				options.Priority = CacheItemPriority.Normal;
+				options.RegisterPostEvictionCallback((key, value, reason, state) =>
+				{
+					_memoryCache.Set("callback", $"{key}->{value} => reason:{reason}");
+				});
+
+				_memoryCache.Set<string>("time", DateTime.Now.ToString(), options);
+			}
+
 			return ActionResultInstance(await _productService.AddAsync(productDto));
 		}
 
@@ -39,6 +76,7 @@ namespace AuthServer.API.Controllers
 		[HttpDelete("{id}")]
 		public async Task<IActionResult> DeleteProduct(int id)
 		{
+			_memoryCache.Remove("time");
 			return ActionResultInstance(await _productService.Remove(id));
 		}
 	}
--- AuthServer.API/Program.cs
+++ AuthServer.API/Program.cs
@@ -74,6 +74,9 @@ builder.Services.AddControllers().AddFluentValidation(options =>
 // Custom validation comes from shared layer.
 builder.Services.UseCustomValidationResponse();
 
+// In-Memory Cache
+builder.Services.AddMemoryCache();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
